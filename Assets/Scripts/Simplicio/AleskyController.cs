d0b3b31f0dc0c03aa6f287ad0e11ae3c8aa49a38
SimplicioLima <49134891+SimplicioLima@users.noreply.github.com>
2022-03-21 23:44:49 +0000

    Alesky Movement

--- Assets/Scripts/Simplicio/AleskyController.cs
+++ Assets/Scripts/Simplicio/AleskyController.cs
@@ -5,43 +5,125 @@ using UnityEngine;
 public class AleskyController : MonoBehaviour
 {
     //Estados do personagem
+    [Header("Player States")]
     private bool _idle = false;
-    private bool _crouching = false;
-    private bool _walking = false;
+    public bool _crouching = false;
+    public bool _walking = false;
     private bool _throwing = false;
     private bool _holdingThrow = false;
 
+    [Space]
+    [Header("Player")]
+    [SerializeField] private Transform player;
+    private Vector3 dir;
+    private Rigidbody rb;
+
+    private float rY;
+    private float rX;
+    private float speed = 10; //Dont touch this
+    [SerializeField] private float speedMultiplier = 20.0f;
 
 
+    [SerializeField] private Transform camPivot;
+    [SerializeField] private Transform cam;
 
 
-    //Movement
-    public float speed = 0.1f;
-    public float rotaionSpeed = 50.0f;
+    //Animator
+    public Animator anim;
 
     void Start()
     {
         //Mouse info
-        Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Movement();
+
+        CheckPlayerState();
+        CheckAnimationWalking();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
     }
 
     private void Movement()
     {
-        if (Input.GetKey(KeyCode.W))
+        dir = player.TransformVector(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized);
+
+        rX = Mathf.Lerp(rX, Input.GetAxisRaw("Mouse X") * 2, 100 * Time.deltaTime);
+        rY = Mathf.Clamp(rY - (Input.GetAxisRaw("Mouse Y") * 2 * 100 * Time.deltaTime), -40, 40); // Valor max de movimento y do mouse 
+
+        player.Rotate(0, rX, 0, Space.World);
+        cam.rotation = Quaternion.Lerp(cam.rotation, Quaternion.Euler(rY * 2, player.eulerAngles.y, 0), 100 * Time.deltaTime);
+
+        //Crouch
+        if (Input.GetKey(KeyCode.LeftControl))
         {
-            transform.Translate(Vector3.forward * speed);
+            _crouching = true;
         }
-        if (Input.GetKey(KeyCode.S))
+
+        //Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            transform.Translate(-Vector3.forward * speed);
+            speed = speedMultiplier;
         }
-        transform.Rotate(0,Input.GetAxis("Mouse X"), 0 * Time.deltaTime * rotaionSpeed);
+        else speed = 10.0f;
+    }
+
+    private void CheckPlayerState()
+    {
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            _walking = false;
+            _idle = true;
+        }
+        else
+        {
+            _walking = true;
+            _idle = false;
+        }
+
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            _crouching = false;
+        }
+    }
+
+    private void CheckAnimationWalking()
+    {
+        if (_walking && _crouching)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isCrouch", true);
+            anim.SetBool("isIdle", false);
+        }
+        else if (_walking && !_crouching)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isCrouch", false);
+            anim.SetBool("isIdle", false);
+
+            //Walking
+            anim.SetFloat("walkingDir", 0.3f);
+        }
+        else if(_crouching && !_walking)
+        {
+            anim.SetBool("isCrouch", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isCrouch", false);
+            anim.SetBool("isIdle", true);
+        }
     }
 }
