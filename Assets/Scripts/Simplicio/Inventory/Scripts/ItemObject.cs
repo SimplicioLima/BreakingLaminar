0ab771d977d1bba9917fe2e9a8fca64e2fb76c20
SimplicioLima <49134891+SimplicioLima@users.noreply.github.com>
2022-04-27 18:13:06 +0100

    Inventario Feito
    Falta ineragir com os obj que podem ser lançados

---
+++ Assets/Scripts/Simplicio/Inventory/Scripts/ItemObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public InventoryItemData referenceItem;
+
+    [SerializeField] private List<ItemRequirement> requirements;
+    public bool removeRequirementsOnPickup;
+
+    public void OnHandlePickupItem()
+    {
+        if (MeetsRequirements())
+        {
+            if (removeRequirementsOnPickup)
+            {
+                RemoveRequirements();
+            }
+
+            InventorySystem.current.Add(referenceItem);
+            Destroy(gameObject);
+        }
+    }
+
+    public bool MeetsRequirements()
+    {
+        foreach (ItemRequirement requirement in requirements)
+        {
+            if(!requirement.HasRequirement()) { return false; }
+        }
+        return true;
+    }
+
+    public bool RemoveRequirements()
+    {
+        foreach (ItemRequirement requirement in requirements)
+        {
+            for (int i = 0; i < requirement.amount; i++)
+            {
+                InventorySystem.current.Remove(requirement.itemData);
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/Simplicio/Inventory/Scripts/ItemRequirement.cs
@@ -0,0 +1,15 @@
+[System.Serializable]
+public struct ItemRequirement
+{
+    public InventoryItemData itemData;
+    public int amount;
+
+    public bool HasRequirement()
+    {
+        InventoryItem item = InventorySystem.current.Get(itemData);
+
+        if (item == null || item.stackSize < amount) { return false; }
+
+        return true;
+    }
+}
